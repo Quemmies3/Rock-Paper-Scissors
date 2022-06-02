6d671658fda3dc8e3ee4389c1e2f7b2723c39f1f
Quemmies3 <quemestree@gmail.com>
2022-06-02 12:52:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# ROCK, PAPER, SCISSORS GAME
+## This is a game of rock, paper, scissors created using python
+
+
+## Rock-Paper-Scissors
+    Rock-Paper-Scissors is a simple two-player game where, at a signal, players make figures with their hands, representing a rock, a piece of paper, or a pair of scissors. The winner is determined according to a set of rules. Please visit [Official Rules], (https://wrpsa.com/the-official-rules-of-rock-paper-scissors/) to learn more about the game and how to win before playing it. 
+
+
+### Prerequisites
+    - Python (latest version)
+    - Code Editor
+
+### Installation
+    * Fork the repo from [Github Source] (https://github.com/Quemmiesi/Rock-Paper-Scissors)
+    * Then clone into your computer using `git clone https://github.com/YOUR_USERNAME/rock-paper-scissors.git`
+    * Navigate into your local repo from the command line using `cd rock-paper-scissors`
+
+### Usage
+Play the game from the command line using `python filename.py`. 
+ >Filename here is what you used to save your .py file (In this repo, it is saved as main.py)
+Similarly you can use `py filename.py`.
+
+### Credits
+This project was done as a task for the fullstack track of the Zuri training put together by the [Zuri Team] (https://training.zuri.team/)
+
+Have a blast playing the game. 
---
+++ main.py
@@ -0,0 +1,84 @@
+#Import module
+import random
+
+# Print multiline instruction
+print("Winning Rules of the Rock paper scissors game as follows: \n"
+            +"Rock vs Paper->Paper wins \n"
+            + "Rock vs Scissors->Rock wins \n"
+            +"Paper vs Scissors->Scissors wins \n"
+            +"If the two players choose the same “character” it’s a tie, and the game repeats. \n")
+
+#create a list of play options
+p = ["R", "P", "S"]
+
+#set player to False
+player = False
+
+while player == False:
+#set player to True
+    print("\nEnter choice \n R for Rock,  \n P for Paper, or  \n S for Scissors\n")
+
+     # take the input from user
+    choice = input("User turn: ")
+
+    # initialize input of choice_name variable
+    if choice == "R":
+        choice_name = 'Rock'
+    elif choice == "P":
+        choice_name = 'Paper'
+    elif choice == "S":
+        choice_name = 'Scissors'
+    else:
+        print("That's not a valid choice. Check your input and put a valid play!")
+        print(input(" "))
+        choice = input("User turn: ")
+        player = False
+
+    # print user choice
+    print("user choice is: " + choice_name)
+    print("\nNow its computer turn.......")
+
+    #assign a random play to the computer
+    computer_choice = random.choice(p)
+
+    #initialize input of computer_choice_name variable
+    if computer_choice== "R":
+        computer_choice_name = 'Rock'
+    elif computer_choice == "P":
+        computer_choice_name = 'Paper'    
+    else:
+        computer_choice_name = 'Scissors'
+
+    print("Computer choice is: " + computer_choice_name + "\n " )
+    print(choice_name + " v/s " + computer_choice_name + "\n")
+    
+    #conditions for Winnung
+    if choice == computer_choice:
+        print("Both players selected"  " "+ choice_name +". It's a tie!!" + "Play again " +"\n")
+    elif choice == "R":
+        if computer_choice == "P":
+            print("You lose!", computer_choice_name, "covers", choice_name , end = "\n")
+            result = "Paper"
+        else:
+            print("You win!", choice_name, "smashes", computer_choice_name, end = "\n" )
+    elif choice == "P":
+        if computer_choice == "S":
+            print("You lose!", computer_choice_name, "cut", choice_name)
+            result = "Scissors"
+        else:
+            print("You win!", choice_name, "covers", computer_choice_name)
+    elif choice == "S":
+        if computer_choice == "R":
+            print("You lose...", computer_choice_name, "smashes", choice_name)
+            result = "Rock"
+        else:
+            print("You win!", choice_name, "cut", computer_choice_name)       
+    
+    print("Do you want to play again? (Y/N)")
+    ans = input()
+
+            # if user input n or N then condition is True
+    if ans == 'n' or ans == 'N':
+            break
+
+print("\nThanks for playing")
